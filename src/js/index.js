f0cc23171e4cfde59080d3898812bf7fbb93c79f
NeoSmait <neosmain@ya.ru>
2022-05-31 16:24:57 +0900

    master

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+document.getElementById('platform').innerHTML = navigator.userAgentData.platform
+document.getElementById('hardwareConcurrency').innerHTML = navigator.hardwareConcurrency
+document.getElementById('deviceMemory').innerHTML = navigator.deviceMemory
