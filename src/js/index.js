b0b3bb70e3e2010fe87e18cdaa3fb1c805c9308e
AlexburtT <73580316+AlexburtT@users.noreply.github.com>
2022-05-31 16:35:50 +0900

    master

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 document.getElementById('platform').innerHTML = navigator.userAgentData.platform
 document.getElementById('hardwareConcurrency').innerHTML = navigator.hardwareConcurrency
 document.getElementById('deviceMemory').innerHTML = navigator.deviceMemory
+console.log(navigator)
