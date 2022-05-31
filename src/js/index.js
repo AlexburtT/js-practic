8fb36a87205cfe4238a393183a38d0dd2a048bd7
NeoSmait <neosmain@ya.ru>
2022-05-31 17:22:52 +0900

    master

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,5 @@
 document.getElementById('platform').innerHTML = navigator.userAgentData.platform
 document.getElementById('hardwareConcurrency').innerHTML = navigator.hardwareConcurrency
 document.getElementById('deviceMemory').innerHTML = navigator.deviceMemory
-<<<<<<< HEAD
-
 
 console.log(alert('Привет!'))
-=======
-console.log(navigator)
->>>>>>> 35439ef04656f90b423dbd708d2c7569526836cd
