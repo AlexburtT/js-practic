46879003809a65734857b0d1fa88d8662a254cda
NeoSmait <neosmain@ya.ru>
2022-06-01 16:30:39 +0900

    master

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
-document.getElementById('platform').innerHTML = navigator.userAgentData.platform;
-document.getElementById('hardwareConcurrency').innerHTML = navigator.hardwareConcurrency;
-document.getElementById('deviceMemory').innerHTML = navigator.deviceMemory;
+document.getElementById('hardwareConcurrency').innerHTML = 'Ваш процессор' + ' ' + navigator.hardwareConcurrency + ' ' + 'ядерный';
+document.getElementById('deviceMemory').innerHTML = 'Оперативной памяти' + ' ' + navigator.deviceMemory;
 
-console.log(navigator.userAgentData);
-console.log(alert('Привет!'));
+navigator.userAgentData
+	.getHighEntropyValues(['architecture', 'bitness', 'brands', 'mobile', 'model', 'platform', 'platformVersion', 'uaFullVersion', 'fullVersionList'])
+	.then(ua => {
+		document.getElementById('platform').innerHTML = navigator.userAgentData.platform + ' ' + ua.platformVersion;
+		document.getElementById('model').innerHTML = 'Ваша модель' + ' ' + ua.model;
+	});
+
+
+// console.log(navigator.userAgentData);
+// console.log(alert('Привет!'));
